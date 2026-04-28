672ef3702694bffcc46cd754aec22e21d6a25766
Dhruv Patel <dhruvthakorpatel05@gmail.com>
2026-04-28 10:39:04 -0500

    initial endpoints

---
+++ main.py
@@ -0,0 +1,44 @@
+# Imports
+from fastapi import FastAPI, Response, Request, HTTPException
+import sqlite3
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class TodoRequest(BaseModel):
+    title: str
+
+# Database Setup
+def get_db():
+    connection = sqlite3.connect("todos.db")
+    connection.execute("""CREATE TABLE IF NOT EXISTS todos(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   title TEXT,
+                   completed INTEGER
+                   )""")
+    connection.commit()
+    return connection
+
+@app.post("/todos")
+async def create_item(body: TodoRequest):
+    connection = get_db()
+    connection.execute("INSERT INTO todos (title, completed) VALUES (?, ?)", (body.title, 0))
+    connection.commit()
+    connection.close()
+    return {"message": "Todo created"}
+
+@app.get("/todos")
+async def get_item():
+    connection = get_db()
+    rows = connection.execute("SELECT * FROM todos").fetchall()
+    connection.close()
+    return [{"id": row[0], "title": row[1], "completed": row[2]} for row in rows]
+
+@app.get("/todos/{id}")
+async def get_item(id: int ):
+    connection = get_db()
+    row = connection.execute("SELECT * FROM todos WHERE id = ?", (id,)).fetchone()
+    connection.close()
+    if not row:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return [{"id": row[0], "title": row[1], "completed": row[2]}]
